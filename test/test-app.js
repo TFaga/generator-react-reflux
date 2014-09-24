e3a82cc2274a4614fafa06eb6560ba2b57f66b59
Tilen Faganel <tilen.faganel@me.com>
2014-09-25 00:29:10 +0200

    Wrote more tests

--- test/test-app.js
+++ test/test-app.js
@@ -7,8 +7,78 @@ var helpers = require('yeoman-generator').test;
 var os = require('os');
 
 describe('react-reflux:app', function () {
-  before(function (done) {
-    helpers.run(path.join(__dirname, '../app'))
+  describe('when using no features', function () {
+    before(function (done) {
+      helpers.run(path.join(__dirname, '../app'))
+      .inDir(path.join(os.tmpdir(), './temp-test'))
+      .withOptions({ 'skip-install': true })
+      .withPrompt({
+        projectName: 'test',
+        desc: 'A test project',
+        author: 'Test Testington',
+        version: '1.0.1',
+        license: 'MIT',
+        features: [
+        ]
+      })
+      .on('end', done);
+    });
+
+    it('creates files', function () {
+      assert.file([ 
+        'app/images',
+        'app/scripts/actions',
+        'app/scripts/stores',
+
+        'app/favicon.ico',
+        'app/robots.txt',
+        'app/scripts/app.js',
+        'app/scripts/router.jsx',
+        'app/scripts/components/layout.jsx',
+        'app/scripts/components/home.jsx',
+        'app/styles/main.css',
+        'app/index.html',
+
+        'gulpfile.js',
+        'bower.json',
+        'package.json',
+        '.editorconfig',
+        '.jshintrc',
+        'README.md'
+      ]);
+    });
+  });
+
+  describe('when using modernizr', function () {
+    before(function (done) {
+      helpers.run(path.join(__dirname, '../app'))
+      .inDir(path.join(os.tmpdir(), './temp-test'))
+      .withOptions({ 'skip-install': true })
+      .withPrompt({
+        projectName: 'test',
+        desc: 'A test project',
+        author: 'Test Testington',
+        version: '1.0.1',
+        license: 'MIT',
+        features: [
+          'includeModernizr'
+        ]
+      })
+      .on('end', done);
+    });
+
+    it('inserts modernizr to html', function () {
+      assert.fileContent('app/index.html', /<script src="vendor\/modernizr\/modernizr.js"><\/script>/);
+    });
+
+    it('inserts modernizr to bower', function () {
+      assert.fileContent('bower.json', /"modernizr"/);
+    });
+  });
+
+  describe('when using compass', function () {
+    before(function (done) {
+      helpers.run(path.join(__dirname, '../app'))
       .inDir(path.join(os.tmpdir(), './temp-test'))
       .withOptions({ 'skip-install': true })
       .withPrompt({
@@ -23,29 +93,30 @@ describe('react-reflux:app', function () {
         ]
       })
       .on('end', done);
-  });
+    });
 
-  it('creates files', function () {
-    assert.file([ 
-      'app/images',
-      'app/scripts/actions',
-      'app/scripts/stores',
+    it('creates sass files', function () {
+      assert.file([
+        'app/styles/main.scss'
+      ]);
+    });
 
-      'app/favicon.ico',
-      'app/robots.txt',
-      'app/scripts/app.js',
-      'app/scripts/router.jsx',
-      'app/scripts/components/layout.jsx',
-      'app/scripts/components/home.jsx',
-      'app/styles/main.scss',
-      'app/index.html',
+    it('doesn\'t create css files', function () {
+      assert.noFile([
+        'app/styles/main.css'
+      ]);
+    });
 
-      'gulpfile.js',
-      'bower.json',
-      'package.json',
-      '.editorconfig',
-      '.jshintrc',
-      'README.md'
-    ]);
+    it('inserts compass to gulp', function () {
+      assert.fileContent('gulpfile.js', /'compass'/);
+    });
+
+    it('inserts modernizr to package.json', function () {
+      assert.fileContent('package.json', /"gulp-compass"/);
+    });
+
+    it('inserts modernizr to home html component', function () {
+      assert.fileContent('app/scripts/components/home.jsx', /<li>Modernizr<\/li>/);
+    });
   });
 });
