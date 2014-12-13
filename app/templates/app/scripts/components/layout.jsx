31103b762e78416c8edfb8c508c79e7fb1adeb57
Tilen Faganel <tilen.faganel@me.com>
2014-12-13 16:48:49 +0100

    Updated react-router javascript syntax to match its latest version

--- app/templates/app/scripts/components/layout.jsx
+++ app/templates/app/scripts/components/layout.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var Router = require('react-router');
+var RouteHandler = Router.RouteHandler;
 
 var Layout = React.createClass({
 
@@ -6,7 +8,7 @@ var Layout = React.createClass({
 
     return (
       <div className="App">
-        <this.props.activeRouteHandler/>
+        <RouteHandler />
       </div>
     );
   }
--- app/templates/app/scripts/router.jsx
+++ app/templates/app/scripts/router.jsx
@@ -1,20 +1,20 @@
 var React = require('react');
 var Router = require('react-router');
 var Route = Router.Route;
-var Routes = Router.Routes;
 var DefaultRoute = Router.DefaultRoute;
 
 var Layout = require('./components/layout');
 var Home = require('./components/home');
 
 var routes = (
-  <Routes>
-    <Route name="layout" path="/" handler={Layout}>
-       <DefaultRoute handler={Home}/>
-    </Route>
-  </Routes>
+	<Route name="layout" path="/" handler={Layout}>
+		<DefaultRoute handler={Home} />
+	</Route>
 );
 
 exports.start = function() {
-  React.renderComponent(routes, document.getElementById('content'));
+  
+  Router.run(routes, function (Handler) {
+		React.render(<Handler />, document.getElementById('content'));
+	});
 }
